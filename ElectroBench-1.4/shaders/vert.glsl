c54087ae8a0e8dc12b56ca696033d121a3d1a502
Ayoub Coding <ayoubprogramming95@gmail.com>
2023-08-02 22:23:51 +0200

    Delete ElectroBench-1.4 directory

--- ElectroBench-1.4/shaders/vert.glsl
+++
@@ -1,9 +0,0 @@
-#version 120
-varying vec3 vNormal;
-varying vec3 vViewDir;
-
-void main() {
-	  gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
-	  vNormal = normalize(gl_NormalMatrix * gl_Normal);
-	  vViewDir = normalize(-gl_Position.xyz);
-}
