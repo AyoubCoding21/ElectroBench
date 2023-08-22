ef98160213b5b5aaacf9aaf055ae1635bd0d9198
Ayoub Coding <ayoubprogramming95@gmail.com>
2023-08-22 18:27:47 +0100

    Update frag.glsl

--- ElectroBench-2.0-beta/shaders/frag.glsl
+++ ElectroBench-2.0-beta/shaders/frag.glsl
@@ -3,14 +3,41 @@ varying vec3 vNormal;
 varying vec3 vViewDir;
 varying vec2 vTexCoord;
 float roughness = 0.8;
-vec2 mod289(vec2 x) { return x - floor(x * (1.0 / 289.0)) * 289.0; }
-vec3 mod289(vec3 x) { return x - floor(x * (1.0 / 289.0)) * 289.0; }
-vec4 mod289(vec4 x) { return x - floor(x * (1.0 / 289.0)) * 289.0; }
-float mod289(float x) { return x - floor(x * (1.0 / 289.0)) * 289.0; }
-vec2 fade(vec2 t) { return t * t * t * (t * (t * 6.0 - 15.0) + 10.0); }
-float taylorInvSqrt(float r) { return 1.79284291400159 - 0.85373472095314 * r; }
-vec4 permute(vec4 x) { return mod289(((x * 34.0) + 10.0) * x); }
-float permute(float x) { return mod289(((x * 34.0) + 10.0) * x); }
+vec3 mod289(vec3 x) {
+  return x - floor(x * (1.0 / 289.0)) * 289.0;
+}
+vec2 mod289(vec2 x) {
+  return x - floor(x * (1.0 / 289.0)) * 289.0;
+}
+vec3 permute(vec3 x) {
+  return mod289(((x*34.0)+10.0)*x);
+}
+vec4 mod289(vec4 x) {
+  return x - floor(x * (1.0 / 289.0)) * 289.0; 
+}
+
+float mod289(float x) {
+  return x - floor(x * (1.0 / 289.0)) * 289.0;
+}
+
+vec4 permute(vec4 x) {
+     return mod289(((x*34.0)+10.0)*x);
+}
+
+float permute(float x) {
+     return mod289(((x*34.0)+10.0)*x);
+}
+
+vec4 taylorInvSqrt(vec4 r)
+{
+  return 1.79284291400159 - 0.85373472095314 * r;
+}
+
+float taylorInvSqrt(float r)
+{
+  return 1.79284291400159 - 0.85373472095314 * r;
+}
+
 vec4 grad4(float j, vec4 ip)
 {
   const vec4 ones = vec4(1.0, 1.0, 1.0, -1.0);
