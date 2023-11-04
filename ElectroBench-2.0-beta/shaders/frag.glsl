f47ec6cc398a248d3077a172a28a37d8a1ab59b2
Ayoub Coding <ayoubprogramming95@gmail.com>
2023-11-04 20:22:19 +0100

    Update frag.glsl

--- ElectroBench-2.0-beta/shaders/frag.glsl
+++ ElectroBench-2.0-beta/shaders/frag.glsl
@@ -449,7 +449,7 @@ float benchNoise(vec2 P)
 {
     float res = 0.0;
     float time = 0.0;
-    for (float i = 0; i < 950000000.0; i += .01)
+    for (float i = 0; i < 950000.0; i += .01)
     {
         res = cellular(vec3(P + time, time + 0.4)).x + 
 	      cellular(vec3(P + time, time + 0.4)).y +
